a7cb0e463d5d7abbbd85dfabe3c9debe74601d9a
Nikolay Imshenitsky <mimshenytskyi@magecore.com>
2014-10-22 14:07:28 +0300

    BAP-5652: Update entity field providers to use serialized field

---
+++ Provider/ConfigSerializedFieldExclusionProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Oro\Bundle\EntitySerializedFieldsBundle\Provider;
+
+use Doctrine\ORM\Mapping\ClassMetadata;
+
+use Oro\Bundle\EntityBundle\Provider\ExclusionProviderInterface;
+use Oro\Bundle\EntityConfigBundle\Provider\ConfigProvider;
+
+/**
+ * The aim of the class is to exclude custom fields with no is_serializable property or if it equals to false
+ */
+class ConfigSerializedFieldExclusionProvider implements ExclusionProviderInterface
+{
+    /** @var ConfigProvider */
+    protected $extendConfigProvider;
+
+    /**
+     * @param ConfigProvider $organizationConfigProvider
+     */
+    public function __construct(ConfigProvider $extendConfigProvider)
+    {
+        $this->extendConfigProvider = $extendConfigProvider;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isIgnoredEntity($className)
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isIgnoredField(ClassMetadata $metadata, $fieldName)
+    {
+        $className = $metadata->getName();
+        if ($this->extendConfigProvider->hasConfig($className, $fieldName)) {
+            $config = $this->extendConfigProvider->getConfig($className, $fieldName);
+            return $config->get('is_serialized', false, false);
+        }
+
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isIgnoredRelation(ClassMetadata $metadata, $associationName)
+    {
+        return false;
+    }
+}
