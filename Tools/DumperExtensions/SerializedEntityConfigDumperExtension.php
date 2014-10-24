273e83d9c09477eb1054ec2cd5a8ed2a58d755bc
Alexander Nezdoiminoga <anezdoiminoga@magecore.com>
2014-10-24 14:15:56 +0300

    BAP-5657: review fixes

--- Tools/DumperExtensions/SerializedEntityConfigDumperExtension.php
+++ Tools/DumperExtensions/SerializedEntityConfigDumperExtension.php
@@ -27,11 +27,7 @@ class SerializedEntityConfigDumperExtension extends AbstractEntityConfigDumperEx
      */
     public function supports($actionType)
     {
-        if ($actionType === ExtendConfigDumper::ACTION_PRE_UPDATE) {
-            return true;
-        }
-
-        return false;
+        return $actionType === ExtendConfigDumper::ACTION_PRE_UPDATE;
     }
 
     /**
@@ -45,26 +41,32 @@ class SerializedEntityConfigDumperExtension extends AbstractEntityConfigDumperEx
         $entityConfigs = $extendConfigProvider->getConfigs();
         foreach ($entityConfigs as $entityConfig) {
             if ($entityConfig->is('is_extend')) {
-                /** @var ConfigInterface[] $entityFields */
-                $entityFields = $extendConfigProvider->getConfigs($entityConfig->getId()->getClassName());
-                $entityFields = array_filter(
-                    $entityFields,
-                    function (ConfigInterface $entityField) {
-                        return $entityField->get('is_serialized', false, false);
-                    }
+                $serializedFields = $extendConfigProvider->filter(
+                    function (ConfigInterface $field) {
+                        return $field->is('is_serialized');
+                    },
+                    $entityConfig->getId()->getClassName()
                 );
 
-                if ($entityFields) {
+                /**
+                 * Because of serialized field(s) data stored in special table column "serialized_data"
+                 * we should not generate doctrine property for such field(s).
+                 * Also we can't index such field(s), so they should not pass into "index" configuration.
+                 */
+                if ($serializedFields) {
                     $schema = $entityConfig->get('schema');
                     $index  = $entityConfig->get('index');
-                    foreach ($entityFields as $entityField) {
-                        unset($schema['property'][$entityField->getId()->getFieldName()]);
-                        unset($schema['doctrine'][$schema['entity']]['fields'][$entityField->getId()->getFieldName()]);
-                        unset($index[$entityField->getId()->getFieldName()]);
+                    foreach ($serializedFields as $serializedField) {
+                        $fieldName = $serializedField->getId()->getFieldName();
+
+                        unset($schema['property'][$fieldName]);
+                        unset($schema['doctrine'][$schema['entity']]['fields'][$fieldName]);
+                        unset($index[$fieldName]);
                     }
 
                     $entityConfig->set('schema', $schema);
                     $entityConfig->set('index', $index ? : []);
+
                     $this->configManager->persist($entityConfig);
                 }
             }
