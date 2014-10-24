f3e8994e1c866788262d1a4e23ddd436c95b1aaf
yurio <yurio@ua.fm>
2014-10-24 18:52:45 +0300

    BAP-5657: Fix Migration query, fix labels

--- Migration/SerializedDataMigrationQuery.php
+++ Migration/SerializedDataMigrationQuery.php
@@ -96,7 +96,6 @@ class SerializedDataMigrationQuery extends ParametrizedMigrationQuery
                                 'array'
                             )
                         );
-
                 }
             }
         }
