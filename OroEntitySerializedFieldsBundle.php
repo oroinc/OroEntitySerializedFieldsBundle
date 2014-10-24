c2e567bdc4b21982487de782dfd28f729eef3741
Nikolay Imshenitsky <mimshenytskyi@magecore.com>
2014-10-24 09:58:54 +0300

    BAP-5652: review fixes

--- OroEntitySerializedFieldsBundle.php
+++ OroEntitySerializedFieldsBundle.php
@@ -3,16 +3,7 @@
 namespace Oro\Bundle\EntitySerializedFieldsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-
-use Oro\Bundle\EntitySerializedFieldsBundle\DependencyInjection\ExclusionProviderCompilerPass;
 
 class OroEntitySerializedFieldsBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $container->addCompilerPass(new ExclusionProviderCompilerPass());
-    }
 }
