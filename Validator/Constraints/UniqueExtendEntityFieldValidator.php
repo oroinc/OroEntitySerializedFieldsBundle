5d0be0f8f17cc961f7b79a3a812e0febebad4f01
Denis Voronin <dvoronin@magecore.com>
2015-10-21 13:33:49 +0300

    BAP-9064: Possible to created extended fields with identical names  - verify special symbols which are deleted during setter or getters names generation

---
+++ Validator/Constraints/UniqueExtendEntityFieldValidator.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Oro\Bundle\EntitySerializedFieldsBundle\Validator\Constraints;
+
+use Doctrine\Common\Inflector\Inflector;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+
+use Oro\Bundle\EntityConfigBundle\Entity\FieldConfigModel;
+
+class UniqueExtendEntityFieldValidator extends ConstraintValidator
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function validate($value, Constraint $constraint)
+    {
+        if (!$value instanceof FieldConfigModel) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    'Oro\Bundle\EntityConfigBundle\Entity\FieldConfigModel supported only, %s given',
+                    is_object($value) ? get_class($value) : gettype($value)
+                )
+            );
+        }
+
+        $newFieldName = strtolower(Inflector::classify(($value->getFieldName())));
+
+        // Need hardcoded check for `serialized_data` field.
+        if ($newFieldName === strtolower(Inflector::classify('serialized_data'))) {
+            $this->addViolation($constraint);
+
+            return;
+        }
+    }
+
+    /**
+     * @param Constraint $constraint
+     */
+    protected function addViolation(Constraint $constraint)
+    {
+        /** @var ExecutionContextInterface $context */
+        $context = $this->context;
+        $context->buildViolation($constraint->message)
+            ->atPath($constraint->path)
+            ->addViolation();
+    }
+}
