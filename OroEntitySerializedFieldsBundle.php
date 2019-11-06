00bf6672b450a8be0c84fb4c504a452d2ea7b0f0
Vova Soroka <vsoroka@oroinc.com>
2019-11-06 17:38:19 +0200

    BB-17019: Get rid of class names in container parameters (#25691)

--- OroEntitySerializedFieldsBundle.php
+++ OroEntitySerializedFieldsBundle.php
@@ -2,8 +2,22 @@
 
 namespace Oro\Bundle\EntitySerializedFieldsBundle;
 
+use Oro\Bundle\EntitySerializedFieldsBundle\DependencyInjection\Compiler\ExtendFieldValidationLoaderPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * The EntitySerializedFieldsBundle bundle class.
+ */
 class OroEntitySerializedFieldsBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new ExtendFieldValidationLoaderPass());
+    }
 }
