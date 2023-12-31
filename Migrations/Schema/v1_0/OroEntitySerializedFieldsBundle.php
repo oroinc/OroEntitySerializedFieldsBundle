82b5587c4b51c110e287d4abd06ee63079aae98d
Vova Soroka <vsoroka@oroinc.com>
2023-12-31 18:11:42 +0200

    BAP-22380: Create a migration query to simplify loading of enum values (#37259)
    - add Oro\Bundle\EntityExtendBundle\Migration\Query\InsertEnumValuesQuery
    - add Oro\Bundle\EntityExtendBundle\Migration\ExtendOptionsManagerAwareInterface and Oro\Bundle\EntityExtendBundle\Migration\ExtendOptionsManagerAwareTrait
    - remove public static methods from some migrations
    - refactor some installers

--- Migrations/Schema/v1_0/OroEntitySerializedFieldsBundle.php
+++ Migrations/Schema/v1_0/OroEntitySerializedFieldsBundle.php
@@ -3,16 +3,17 @@
 namespace Oro\Bundle\EntitySerializedFieldsBundle\Migrations\Schema\v1_0;
 
 use Doctrine\DBAL\Schema\Schema;
+use Oro\Bundle\EntitySerializedFieldsBundle\Migrations\Schema\UpdateCustomFieldsWithStorageType;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
 class OroEntitySerializedFieldsBundle implements Migration
 {
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
-    public function up(Schema $schema, QueryBag $queries)
+    public function up(Schema $schema, QueryBag $queries): void
     {
-        $queries->addQuery(new UpdateCustomFieldsWithStorageType($schema));
+        $queries->addQuery(new UpdateCustomFieldsWithStorageType());
     }
 }
